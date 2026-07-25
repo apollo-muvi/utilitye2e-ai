16fe883bd2856e84c0a511c1ca89b6b0eb0cd1c1
apollo-muvi <apollo-muvi@users.noreply.github.com>
2026-07-25 19:25:51 +0800

    feat: add startup scripts for mock and web servers

---
+++ start-mock.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/one/utilitye2e-ai
+source /home/one/utilitye2e/venv/bin/activate
+python3 mock_server.py
---
+++ start-web.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/one/utilitye2e-ai
+source /home/one/utilitye2e/venv/bin/activate
+export PYTHONPATH=/home/one/utilitye2e-ai
+python3 web/app.py
