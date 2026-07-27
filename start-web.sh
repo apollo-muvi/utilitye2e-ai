e6a1d91b0d00b0d47bdb823c63055cb5b521f47c
apollo-muvi <apollo.fadudu@gmail.com>
2026-07-27 19:18:15 +0800

    feat: Add requirements.txt and setup independent venv
    - Add requirements.txt with all dependencies (Flask, Playwright, psycopg2, etc.)
    - Update .gitignore to exclude venv/ and utilitye2e_ai.egg-info/
    - Update start-web.sh to use local venv instead of shared one

--- start-web.sh
+++ start-web.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/one/utilitye2e-ai
-source /home/one/utilitye2e/venv/bin/activate
-export PYTHONPATH=/home/one/utilitye2e-ai
+cd /home/apollo/utilitye2e-ai
+source venv/bin/activate
+export PYTHONPATH=/home/apollo/utilitye2e-ai
 python3 web/app.py
